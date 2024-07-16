29bcce815601a702e2c0dc9b2139880c03d732f9
IsmailOulladi <ismailoulladi88@gmail.com>
2024-07-16 12:44:28 +0100

    fetching

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState} from 'react';
+import '../App.css';
 
-const Contact = () => {
-  return (
-    <div className="container">
-      <h2>Contact Us</h2>
-      <p>
-      This is contact US page   
-      </p>
-    </div>
-  );
-};
+function Contact() {
+  const [users, setUsers] = useState([]);
+
+ 
+    fetch('https://hackathoran-2024-backend.onrender.com/api/users')
+      .then(response => response.json())
+      .then(data => setUsers(() => data.content))
+      .catch(console.log("Erreur fetching"));
+console.log(users);      
+      
+return (
+  <>
+  {users.map(user => <p> {user.name}
+                        {user.age}
+                        {user.phone}</p>)}
+                        </>
+)
+
+  
+
+  
+}
 
 export default Contact;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,13 @@
 // src/components/Home.js
-import React, { useContext } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
 
 function Home() {
-  const { theme, setTheme } = useContext(ThemeContext);
+  const [ count , setCount] = useState(0);
 
   return (
-    <div className={`home-container ${theme}`}>
+    <div className={`home-container`}>
       <div className="home-content">
         <h1 className="page-title">Welcome to the National Association of Sciences and Sustainable Development</h1>
         <p>Our mission is to promote science and sustainable development...</p>
@@ -16,8 +15,8 @@ function Home() {
           <button className="btn-primary">Learn More</button>
         </Link>
         <div>
-          <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-            Toggle Theme
+          <button onClick={() => setCount(count + 1)}>
+            Count {count}
           </button>
         </div>
       </div>
